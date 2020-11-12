cec9cbd82472abe2847a8f00b34bc5a06f086abb
nathan ganesh <489290@student.saxion.nl>
2020-11-12 12:12:29 +0100

    final push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+	return (
+		<div className="question-box">
+			<div className="question-title">
+				<h2>Questions And Answers About Login</h2>
+			</div>
+			<div className="question-item">
+				{data.map((item) => {
+					return <SingleQuestion data={item} key={item.id} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ data }) => {
+	const { title, info } = data;
+	const [ toggleButton, setToggleButton ] = React.useState(false);
+	// console.log(data);
+	return (
+		<section className="section-item">
+			<div className="section-button">
+				<h3>{title}</h3>
+				<button className="toggle-btn" onClick={() => setToggleButton(!toggleButton)}>
+					{toggleButton ? <AiOutlineMinus /> : <AiOutlinePlus />}
+				</button>
+			</div>
+			<div className="info">{toggleButton && <h4>{info}</h4>}</div>
+		</section>
+	);
 };
 
 export default Question;
